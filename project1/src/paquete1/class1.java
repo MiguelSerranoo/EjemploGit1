da54c55fafcf51f733e70fa773c68c6c205985af
Migue <Migue@PCmiguel>
2024-04-10 13:23:13 +0200

    primera fase

---
+++ project1/src/paquete1/class1.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("estamos haciendocontrol de versiones en git");
+	}
+
+}
