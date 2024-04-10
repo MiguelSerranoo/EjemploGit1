328a92c3a937555cb6346ba148a846a9ccc43429
Migue <Migue@PCmiguel>
2024-04-10 13:26:22 +0200

    segudna fase

--- project1/src/paquete1/class1.java
+++ project1/src/paquete1/class1.java
@@ -3,10 +3,11 @@ package paquete1;
 public class class1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 		
 		System.out.println("estamos haciendocontrol de versiones en git");
+		System.out.println("Segudna liena de codigo");
 	}
 
 }
