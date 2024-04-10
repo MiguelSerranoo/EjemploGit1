b2de41ea0f0a9980a65fe498f6474fd425a1894e
Migue <Migue@PCmiguel>
2024-04-10 13:27:16 +0200

    tercera fase

--- project1/src/paquete1/class1.java
+++ project1/src/paquete1/class1.java
@@ -4,9 +4,9 @@ public class class1 {
 
 	public static void main(String[] args) {
 	
-
 		
-		System.out.println("estamos haciendocontrol de versiones en git");
+		
+		System.out.println("estamos haciendocontrol en git");
 		System.out.println("Segudna liena de codigo");
 	}
 
